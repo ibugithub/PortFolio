a19ede99cda41f50f92f16ff23505db2291869f5
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-11-12 21:10:50 +0600

    Add header

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,10 +29,10 @@ const  Hero = () => {
           <div className="-mt-36">
             <div ref={ref} className="flex cursor-default flex-col space-y-2">
               <h1 className="text-5xl font-semibold sm:text-7xl md:text-8xl xl:text-9xl">
-                Ibrahim ibu
+                Ibrahim 
               </h1>
               <h2 className="text-3xl font-medium opacity-80 sm:text-6xl md:text-6xl xl:text-7xl">
-                I build things for the web.
+                A Full-Stack Web Developer.
               </h2>
               <Link
                 href="/about"
