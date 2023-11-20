017da58268bbf7a58984b63e07959651ba3e9371
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-11-20 10:33:44 +0600

    Solve Tilt div can not be a child .. issue

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -30,7 +30,7 @@ const services = [
 
 
 const ServiceCard = ({ index, title, icon }: {index : number, title : string, icon : StaticImageData }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt className='xs:w-[250px] w-full' >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
