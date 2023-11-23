a1819d9cbd0258f237a760d89a5ea79b081f44a9
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-11-23 18:50:57 +0600

    Fix position of about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,66 +1,23 @@
 import React from "react";
 import { styles } from "./styles";
-import { fadeIn, textVariant } from 'lib/utils/motion'
-import  web from 'public/static/images/web.png'
-import  mobile from 'public/static/images/mobile.png'
-import backend  from 'public/static/images/backend.png'
-import creator  from 'public/static/images/creator.png'
-import Image, { StaticImageData } from "next/image";
-
-const services = [
-  {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
-  {
-    title: "Backend Developer",
-    icon: backend,
-  },
-  {
-    title: "Content Creator",
-    icon: creator,
-  },
-];
-
-
-const ServiceCard = ({ index, title, icon }: {index : number, title : string, icon : StaticImageData }) => (
-      <div
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <Image
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
-);
 
 const About = () => {
   return (
-    <>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+    <div className="mx-auto w-screen max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0">
+    
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
 
+      <p className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
 
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
-    </>
+      </p>
+
+    </div>
   );
 };
 
