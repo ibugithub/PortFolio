248fd883e08a7fba1e7ed69262a4f0685bf4375c
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-11-24 18:50:07 +0600

    Fix apostrophi problem

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -9,11 +9,11 @@ const About = () => {
       <h2 className={styles.sectionHeadText}>Overview.</h2>
 
       <p className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-        I'm a skilled software developer with experience in TypeScript and
+        I&apos;m a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        Three.js. I&apos;m a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        real-world problems. Let&apos;s work together to bring your ideas to life!
 
       </p>
 
