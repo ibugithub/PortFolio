2890dc29d7124db3bb0b40221f3c64009d3fe2fe
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-11-30 20:56:25 +0600

    Add top and bottom margin to tech section

--- components/Tech/Tech.tsx
+++ components/Tech/Tech.tsx
@@ -5,14 +5,19 @@ import { technologies } from './constant'
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10 mx-auto w-screen max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0 mb-16'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <Image src={technology.icon} alt="this is a tech image"/>
-        </div>
-      ))}
+    <div className="mx-auto w-screen max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0 mb-16">
+      <h2 className="text-[35px] font-bold">Tech Stack</h2>
+
+      <div className='flex flex-row flex-wrap justify-center gap-10 mt-10 mb-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <Image src={technology.icon} alt="this is a tech image" />
+          </div>
+        ))}
+      </div>
     </div>
+
   );
 };
 
-export default Tech ;
+export default Tech;
