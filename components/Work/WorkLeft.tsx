2111bea818b2e9565a01ede5f0537905e39c4b9c
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-12-05 20:28:50 +0600

    remove translation y to fix the work content position

--- components/Work/WorkLeft.tsx
+++ components/Work/WorkLeft.tsx
@@ -10,7 +10,6 @@ export function WorkLeft({ children, progress }: WorkProps) {
   return (
     <div
       className="flex h-[30vh] flex-col items-center justify-center text-3xl lg:h-auto lg:text-3xl"
-      style={{ transform: `translateY(${translateY}px)` }}
     >
       <div className="leading-10 text-white">{children}</div>
     </div>
