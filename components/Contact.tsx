ed44f89ac3aaf0936c09265aefed438717fa05cd
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-12-10 08:46:12 +0600

    Add FromsPre on contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
 
   return (
     <>
-      <form className="flex flex-col gap-4 justify-center items-center mt-32 text-black ff">
+      <form className="flex flex-col gap-4 justify-center items-center mt-32 text-black ff" method="POST" action="https://formspree.io/f/moqzkwvy">
         <div className="max-w-md text-center flex flex-col gap-4">
           <div className="text-emerald-500 text-5xl font-bold"><h3> Send me a message !</h3></div>
           <div className="text-emerald-900 text-2xl">Got a question or proposal, or just want to say hello ? Go ahead.</div>
@@ -14,21 +14,21 @@ const Contact = () => {
           <div className="flex justify-between pt-12 ">
 
             <div className="flex flex-col w-2/5">
-              <label className="text-emerald-900 " htmlFor="name">Your Name:</label>
-              <input  className="border-green-500 outline-none " type="text" id="name" placeholder="Enter Your Name"
+              <label className="text-emerald-900 "  htmlFor="name" >Your Name:</label>
+              <input  className="border-green-500 outline-none " type="text" id="name" name='name' placeholder="Enter Your Name"
               />
             </div>
 
             <div className="flex flex-col w-2/5">
               <label className="text-emerald-900" htmlFor="email">Email Address:</label>
-              <input className="border-green-500" type="text" id="email" placeholder="Enter Your Email Address" />
+              <input className="border-green-500" type="text" id="email" name='email' placeholder="Enter Your Email Address" />
             </div>
 
           </div>
 
           <div className="flex flex-col pt-5">
             <label className="text-emerald-900" htmlFor="message">Your Message:</label>
-            <textarea className="border-green-500" id="message" placeholder="Hi, I think we need a webApp for our products at Company X. How soon can you hop on to discuss this?" />
+            <textarea className="border-green-500" id="message" placeholder="Hi, I think we need a webApp for our products at Company X. How soon can you hop on to discuss this?" name="message" />
           </div>
 
         </div>
